29520c218c0d7645e1894732c5a592ebe4def3f6
antoinerobin <antoine.robin@vesperiagroup.com>
2015-01-28 10:17:47 +0100

    The test content is now stored in a file instead of in the ontology

--- model/TestModel.php
+++ model/TestModel.php
@@ -70,6 +70,18 @@ class TestModel
      * @see taoTests_models_classes_TestModel::onTestModelSet()
      */
     public function deleteContent( core_kernel_classes_Resource $test) {
+        $propInstanceContent = new core_kernel_classes_Property(TEST_TESTCONTENT_PROP);
+        /** @var \core_kernel_classes_Literal $directoryId */
+        $directoryId = $test->getOnePropertyValue($propInstanceContent);
+        if(is_null($directoryId)){
+            throw new \common_exception_FileSystemError(__('Unknown test directory'));
+        }
+
+        $directory = \tao_models_classes_service_FileStorage::singleton()->getDirectoryById($directoryId->literal);
+        if(is_dir($directory->getPath())){
+            \tao_helpers_File::delTree($directory->getPath());
+        }
+
 		$test->removePropertyValues(new core_kernel_classes_Property(TEST_TESTCONTENT_PROP));
     }
     
@@ -79,9 +91,25 @@ class TestModel
      */
     public function getItems( core_kernel_classes_Resource $test) {
         $propInstanceContent = new core_kernel_classes_Property(TEST_TESTCONTENT_PROP);
-        $json = $test->getOnePropertyValue($propInstanceContent);
+        //get the DirectoryId
+        $directoryId = $test->getOnePropertyValue($propInstanceContent);
+        if(is_null($directoryId)){
+            throw new \common_exception_FileSystemError(__('Unknown test directory'));
+        }
+        $directory = \tao_models_classes_service_FileStorage::singleton()->getDirectoryById($directoryId->literal);
+
+
         $items = array();
-        if (!is_null($json)) {
+        if (!is_null($directory)) {
+            $file = $directory->getPath().'content.json';
+            //get the content of file or the encoded items if it's an old test
+            if(is_dir($directory->getPath())){
+                $json = file_get_contents($file);
+            }
+            else{
+                $json = $directoryId;
+            }
+
             $decoded = json_decode($json, true);
             if (is_array($decoded)) {
                 foreach ($decoded as $uri) {
@@ -100,9 +128,43 @@ class TestModel
      * @see taoTests_models_classes_TestModel::cloneContent()
      */
     public function cloneContent( core_kernel_classes_Resource $source, core_kernel_classes_Resource $destination) {
-		$propInstanceContent = new core_kernel_classes_Property(TEST_TESTCONTENT_PROP);
-        $value = $source->getOnePropertyValue($propInstanceContent);
-        $destination->editPropertyValues($propInstanceContent, $value);
+
+        $propInstanceContent = new core_kernel_classes_Property(TEST_TESTCONTENT_PROP);
+        //get the source DirectoryId
+        $sourceDirectoryId = $source->getOnePropertyValue($propInstanceContent);
+
+        if(is_null($sourceDirectoryId)){
+            throw new \common_exception_FileSystemError(__('Unknown test directory'));
+        }
+        //get the real directory (or the encoded items if an old test)
+        $directory = \tao_models_classes_service_FileStorage::singleton()->getDirectoryById($directoryId->literal);
+
+        //an old test so create the content.json to copy
+        if(!is_dir($directory->getPath())){
+            $directory = \tao_models_classes_service_FileStorage::singleton()->spawnDirectory(true);
+            $file = $directory->getPath().'content.json';
+            file_put_contents($file, json_encode($directoryId));
+        }
+
+        $destDirectoryId = $destination->getOnePropertyValue($propInstanceContent);
+
+        if(is_null($destDirectoryId)){
+            //create the destination directory
+            $destDirectory = \tao_models_classes_service_FileStorage::singleton()->spawnDirectory(true);
+        }
+
+        //get the real directory (or the encoded items if an old test)
+        $destDirectory = \tao_models_classes_service_FileStorage::singleton()->getDirectoryById($directoryId->literal);
+
+        //an old test so create the directory
+        if(!is_dir($directory->getPath())){
+            $destDirectory = \tao_models_classes_service_FileStorage::singleton()->spawnDirectory(true);
+        }
+
+
+        \tao_helpers_File::copy($directory->getPath(), $destDirectory->getPath(), true);
+
+        $destination->editPropertyValues($propInstanceContent, $destDirectory->getId());
     }
     
     /**
@@ -129,6 +191,23 @@ class TestModel
      */
     public function save(core_kernel_classes_Resource $test, array $itemUris) {
         $propInstanceContent = new core_kernel_classes_Property(TEST_TESTCONTENT_PROP);
-        return $test->editPropertyValues($propInstanceContent, json_encode($itemUris));
+        //get Directory ID
+        $directoryId = $test->getOnePropertyValue($propInstanceContent);
+
+        //null so create one
+        if(is_null($directoryId)){
+            $directory = \tao_models_classes_service_FileStorage::singleton()->spawnDirectory(true);
+        }
+        else{
+            //get the real directory (or the encoded items if an old test)
+            $directory = \tao_models_classes_service_FileStorage::singleton()->getDirectoryById($directoryId->literal);
+            if(!is_dir($directory->getPath())){
+                //create a new directory if items are stored in content
+                $directory = \tao_models_classes_service_FileStorage::singleton()->spawnDirectory(true);
+            }
+        }
+        $file = $directory->getPath().'content.json';
+        file_put_contents($file, json_encode($itemUris));
+        return $test->editPropertyValues($propInstanceContent, $directory->getId());
     }
 }
