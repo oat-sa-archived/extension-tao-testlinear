910ea271662ae3e5398a65e8c8703cee5ed7a4c3
Ionut Padurariu <ionut@taotesting.com>
2017-12-14 14:18:24 +0100

    wrong extension number

--- manifest.php
+++ manifest.php
@@ -28,7 +28,7 @@ return array(
     'version' => '3.1.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
-        'tao' => '>=14.3.1',
+        'tao' => '>=12.21.4',
         'taoTests' => '>=4.0.0'
     ),
     'managementRole' => 'http://www.tao.lu/Ontologies/generis.rdf#taoTestLinearManager',
